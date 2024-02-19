5becfdabc7f8a279f83082f415538210f4e28772
Ikbal Hossain <ikbalhossain125@gmail.com>
2024-02-19 09:35:32 +0600

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fuck</title>
+    <title>frontend</title>
 </head>
 <body>
     <h1>This is Frontend</h1>
