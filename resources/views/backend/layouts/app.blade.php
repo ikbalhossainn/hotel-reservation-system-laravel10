d685d27869d9a1c30424e5214d1f87a3bb435051
Ikbal Hossain <ikbalhossain125@gmail.com>
2024-02-21 11:17:04 +0600

    asset

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -8,19 +8,19 @@
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico"')}}>
 
         <!-- jvectormap -->
-        <link href="assets/libs/jqvmap/jqvmap.min.css" rel="stylesheet" />
+        <link href="{{asset('assets/libs/jqvmap/jqvmap.min.css')}}" rel="stylesheet" />
 
         <!-- DataTables -->
-        <link href="assets/libs/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/libs/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('assets/libs/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('assets/libs/datatables/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css"/>
         
         <!-- App css -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('assets/css/icons.min.css" rel="stylesheet')}}" type="text/css" />
+        <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
 
     </head>
 
@@ -64,28 +64,28 @@
         <div class="rightbar-overlay"></div>
 
         <!-- Vendor js -->
-        <script src="assets/js/vendor.min.js"></script>
+        <script src="{{asset('assets/js/vendor.min.js')}}"></script>
 
         <!-- KNOB JS -->
-        <script src="assets/libs/jquery-knob/jquery.knob.min.js"></script>
+        <script src="{{asset('assets/libs/jquery-knob/jquery.knob.min.js')}}"></script>
         <!-- Chart JS -->
-        <script src="assets/libs/chart-js/Chart.bundle.min.js"></script>
+        <script src="{{asset('assets/libs/chart-js/Chart.bundle.min.js')}}"></script>
 
         <!-- Jvector map -->
-        <script src="assets/libs/jqvmap/jquery.vmap.min.js"></script>
-        <script src="assets/libs/jqvmap/jquery.vmap.usa.js"></script>
+        <script src="{{asset('assets/libs/jqvmap/jquery.vmap.min.js')}}"></script>
+        <script src="{{asset('assets/libs/jqvmap/jquery.vmap.usa.js')}}"></script>
         
         <!-- Datatable js -->
-        <script src="assets/libs/datatables/jquery.dataTables.min.js"></script>
-        <script src="assets/libs/datatables/dataTables.bootstrap4.min.js"></script>
-        <script src="assets/libs/datatables/dataTables.responsive.min.js"></script>
-        <script src="assets/libs/datatables/responsive.bootstrap4.min.js"></script>
+        <script src="{{asset('assets/libs/datatables/jquery.dataTables.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables/dataTables.bootstrap4.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables/dataTables.responsive.min.js')}}"></script>
+        <script src="{{asset('assets/libs/datatables/responsive.bootstrap4.min.js')}}"></script>
         
         <!-- Dashboard Init JS -->
-        <script src="assets/js/pages/dashboard.init.js"></script>
+        <script src="{{asset('assets/js/pages/dashboard.init.js')}}"></script>
         
         <!-- App js -->
-        <script src="assets/js/app.min.js"></script>
+        <script src="{{asset('assets/js/app.min.js')}}"></script>
 
     </body>
 </html>
