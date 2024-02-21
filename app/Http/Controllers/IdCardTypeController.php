bc5439b0cbd5089053b857a294b8ddcbeed1d92d
Ikbal Hossain <ikbalhossain125@gmail.com>
2024-02-21 16:38:33 +0600

    Model_Controller

---
+++ app/Http/Controllers/IdCardTypeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IdCardType;
+use Illuminate\Http\Request;
+
+class IdCardTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(IdCardType $idCardType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(IdCardType $idCardType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, IdCardType $idCardType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(IdCardType $idCardType)
+    {
+        //
+    }
+}
